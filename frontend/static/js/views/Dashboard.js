eddda0be23a91a82b0e6df44cd10032d9121c4fb
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-03-14 01:25:14 +0000

    added dashboard view and index.css + fixing font bug

---
+++ frontend/static/js/views/Dashboard.js
@@ -0,0 +1,17 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super()
+        this.setTitle('Dashboard')        
+    }
+    
+    async getHtml() {
+        return `
+            <h1>Welcome back, Dom</h1>
+            <p>Esse in consectetur labore nisi laborum ad anim sunt laboris veniam in sint anim. Quis fugiat laborum do exercitation fugiat pariatur Lorem.</p>
+            <p><a href='/posts' data-link>View recent posts</a></p>
+        ` 
+        
+    }
+}
